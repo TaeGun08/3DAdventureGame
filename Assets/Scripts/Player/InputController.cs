d4621b7c527283d3106f9b29fb952b4bd7c7e0f7
Hyuns008 <gustn4984@naver.com>
2024-03-07 15:16:19 +0900

    공격 모션 추가

--- Assets/Scripts/Player/InputController.cs
+++ Assets/Scripts/Player/InputController.cs
@@ -11,31 +11,52 @@ public class InputController : MonoBehaviour
 
     private Animator anim; //플레이어의 애니메이션을 받아올 변수
 
-    [Header("플레이어 설정")]
-    [SerializeField, Range(0, 1), Tooltip("Idle 애니메이션 변경")] private int idleChange;
-    [Space]
-    [SerializeField, Tooltip("플레이어의 이동속도")] private float moveSpeed;
-    [Space]
+    [Header("플레이어 애니메이션 변경")]
+    [SerializeField, Range(0, 1)] private int idleChange;
+
+    [Header("플레이어 이동속도")]
+    [SerializeField] private float moveSpeed;
+
+    [Header("플레이어 스테미너")]
+    [SerializeField] private float maxStamina;
+    [SerializeField] private float curStamina;
+
+    [Header("플레이어 구르기")]
     [SerializeField, Tooltip("플레이어의 구르기 힘")] private float diveRollForce;
     [SerializeField, Tooltip("구르기 쿨타임")] private float diveRollCoolTime;
-    private Transform diveRollTrs; //구르기 시 회전할 방향을 받아올 변수
     private float diveRollTimer; //구르기 쿨타임을 적용할 타이머 변수
-    private float deveRollValue;
-    private float dive;
     private bool useDieveRoll = false; //구르기를 사용했는지 체크하기 위한 변수
+
+    //공격 모션을 위한 변수들
+    private bool isAttack = false; //공격을 했는지 여부를 확인하기 위한 변수
+    private float attackTimer; //공격 후 딜레이를 위해 돌아가는 타이머
+    private float attackDelay; //딜레이 시간
+    private int attackCount; //몇 번째 공격을 했는지 체크하기 위한 변수
+    private bool attackCombo; //콤보 어택을 위한 변수
+    private float comboTimer; //콤보 어택을 위한 시간
+    private float changeStaminaAttack; //공격모션을 변경하기 위한 변수
+
+    [Header("플레이어의 무기 설정")]
+    [SerializeField] private Transform playerHandTrs; //플레이어의 손 위치
+    [SerializeField] private Transform playerBackTrs; //플레이어의 등 위치
+    [SerializeField] private GameObject weapon; //플레이어의 무기
+    private float weaponChangeDelay; //플레이어의 손과 무기를 변경하기 위한 딜레이 시간
+    private bool weaponChange = false; //플레이어가 무기를 변경하였는지 체크하기 위한 변수
 
     private void Awake()
     {
         characterController = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
-        diveRollTrs = GetComponent<Transform>();
-
         Cursor.lockState = CursorLockMode.Locked;
+
+        diveRollTimer = diveRollCoolTime;
     }
 
     private void Start()
     {
         mainCam = Camera.main;
+
+        curStamina = maxStamina;
     }
 
     private void Update()
@@ -43,7 +64,10 @@ public class InputController : MonoBehaviour
         playerTimers();
         playerLookAtScreen();
         playerMove();
+        playerStamina();
         playerDiveRoll();
+        playerWeaponChange();
+        playerAttack();
         playerAnim();
     }
 
@@ -59,6 +83,41 @@ public class InputController : MonoBehaviour
             {
                 diveRollTimer = diveRollCoolTime;
                 useDieveRoll = false;
+            }
+        }
+
+        if (isAttack == true)
+        {
+            attackTimer += Time.deltaTime;
+            if (attackTimer >= attackDelay)
+            {
+                if (attackCount < 1)
+                {
+                    attackCombo = true;
+                }
+                attackTimer = 0f;
+                isAttack = false;
+            }
+        }
+
+        if (attackCombo == true)
+        {
+            comboTimer += Time.deltaTime;
+            if (comboTimer >= 0.3f)
+            {
+                comboTimer = 0f;
+                attackCount = 0;
+                attackCombo = false;
+            }
+        }
+
+        if (weaponChange == true)
+        {
+            weaponChangeDelay += Time.deltaTime;
+            if (weaponChangeDelay >= 1f)
+            {
+                weaponChangeDelay = 0f;
+                weaponChange = false;
             }
         }
     }
@@ -79,6 +138,11 @@ public class InputController : MonoBehaviour
         if (useDieveRoll == true)
         {
             characterController.Move(transform.rotation * new Vector3(0f, 0f, diveRollForce) * Time.deltaTime);
+            return;
+        }
+
+        if (isAttack == true)
+        {
             return;
         }
 
@@ -110,15 +174,128 @@ public class InputController : MonoBehaviour
     }
 
     /// <summary>
+    /// 플레이어의 스테미너를 제어하기 위한 함수
+    /// </summary>
+    private void playerStamina()
+    {
+        if (curStamina < maxStamina)
+        {
+            curStamina += 10f * Time.deltaTime;
+        }
+        else if (curStamina > maxStamina)
+        {
+            curStamina = maxStamina;
+        }
+
+        if (curStamina < 0)
+        {
+            curStamina = 0f;
+        }
+    }
+
+    /// <summary>
     /// 구르기(회피)를 담당하는 함수
     /// </summary>
     private void playerDiveRoll()
     {
-        deveRollValue = Mathf.SmoothDampAngle(transform.localRotation.y, -90f, ref dive, 3f, 4f);
-        if (Input.GetKeyDown(KeyCode.Space) && useDieveRoll == false)
+        if (Input.GetKeyDown(KeyCode.Space) && useDieveRoll == false && curStamina > 30f)
         {
+            Debug.Log("구르기");
             anim.Play("Unarmed-DiveRoll-Forward1");
+            curStamina -= 30;
             useDieveRoll = true;
+        }
+    }
+
+    /// <summary>
+    /// 플레이어가 무기와 손을 변경하기 위한 함수
+    /// </summary>
+    private void playerWeaponChange()
+    {
+        if (Input.GetKeyDown(KeyCode.Q) && weapon != null && weaponChange == false)
+        {
+            if (idleChange == 0)
+            {
+                idleChange = 1;
+            }
+            else
+            {
+                idleChange = 0;
+            }
+
+            weaponChange = true;
+        }
+    }
+
+    /// <summary>
+    /// 플레이어의 공격을 담당하는 함수
+    /// </summary>
+    private void playerAttack()
+    {
+        if (useDieveRoll == true)
+        {
+            isAttack = true;
+            attackDelay = 0.3f;
+            attackCount = 0;
+            attackCombo = false;
+            attackTimer = 0f;
+            comboTimer = 0f;
+            return;
+        }
+
+        if (isAttack == false && attackCombo == false && attackCount > 0)
+        {
+            attackCount = 0;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && isAttack == false)
+        {
+            if (idleChange == 0)
+            {
+                if (attackCombo == true)
+                {
+                    attackCount = 1;
+                    comboTimer = 0f;
+                    attackCombo = false;
+                }
+
+                anim.Play("Attack Tree");
+                attackDelay = 1f;
+                isAttack = true;
+            }
+            else
+            {
+                changeStaminaAttack = 0;
+
+                if (attackCombo == true)
+                {
+                    attackCount = 1;
+                    comboTimer = 0f;
+                    attackCombo = false;
+                }
+
+                anim.Play("Attack Tree");
+                attackDelay = 1f;
+                isAttack = true;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Mouse1) && isAttack == false
+            && idleChange == 1 && curStamina > 30f)
+        {
+            changeStaminaAttack = 1;
+
+            if (attackCombo == true)
+            {
+                attackCount = 1;
+                comboTimer = 0f;
+                attackCombo = false;
+            }
+
+            anim.Play("Attack Tree");
+            attackDelay = 1f;
+            isAttack = true;
+
+            curStamina -= 30f;
         }
     }
 
@@ -130,5 +307,8 @@ public class InputController : MonoBehaviour
         anim.SetFloat("VeticalMove", inputVertical());
         anim.SetFloat("HorizontalMove", inputHorizontal());
         anim.SetFloat("IdleChange", idleChange);
+        anim.SetFloat("ChangeAttack", idleChange);
+        anim.SetFloat("AttackCount", attackCount);
+        anim.SetFloat("StaminaAttack", changeStaminaAttack);
     }
 }
