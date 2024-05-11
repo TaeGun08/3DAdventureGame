2c7fff1a880872e3f1172c54788278fc9e9f6d83
Hyuns008 <gustn4984@naver.com>
2024-05-11 18:51:01 +0900

    UI화면 밖으로 벗어나는 거 막는 중

--- Assets/Scripts/UI/UIDrag.cs
+++ Assets/Scripts/UI/UIDrag.cs
@@ -16,19 +16,31 @@ public class UIDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
     private UITransform uITransform = new UITransform();
 
     [SerializeField] private RectTransform parentRectTrs;
-    private Vector2 mousePos;
+    [SerializeField] private Vector2 mousePos;
 
     [SerializeField] private string setKeyName;
 
+    [SerializeField] private float screenWidth;
+    [SerializeField] private float screenHeight;
+
     void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
     {
-        mousePos.x = eventData.position.x;
-        mousePos.y = eventData.position.y;
+        if (screenWidth == 0)
+        {
+            screenWidth = Screen.width;
+            screenHeight = Screen.height;
+        }
+
+        mousePos.x = transform.position.x - eventData.position.x;
+        mousePos.y = transform.position.y - eventData.position.y;
     }
 
     void IDragHandler.OnDrag(PointerEventData eventData)
     {
-        parentRectTrs.position = eventData.position;
+        parentRectTrs.position = eventData.position + mousePos;
+
+        //width와 height를 이용하여 UI를 화면 밖으로 못 나가게 만들어야 함
+        
     }
 
     void IEndDragHandler.OnEndDrag(PointerEventData eventData)
@@ -49,9 +61,5 @@ public class UIDrag : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHa
 
             parentRectTrs.position = new Vector2(uITransform.xPos, uITransform.yPos);
         }
-    }
-
-    private void Update()
-    {
     }
 }
